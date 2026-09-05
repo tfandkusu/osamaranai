da6a3633985bf5cdf652fb46078ab4c982e3410e
Haruhiko Takada <haruhiko.takada@gmail.com>
2026-09-05 16:58:59 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# osamaranai
+
+FlutterKaigi 2026 セッション 「収まらない」を仕様にする 調査用 Flutter プロジェクト
---
+++ android/app/src/main/kotlin/com/tfandkusu/osamaranai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tfandkusu.osamaranai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
